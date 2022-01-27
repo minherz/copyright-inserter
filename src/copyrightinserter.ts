e17aa0873823c345c638d3f78c585e0e3e033fb8
Hemant Sachdeva <hemant.evolver@gmail.com>
2022-01-27 23:21:20 +0530

    Added AGPL 3 (#16)
    * Added Affero GPL 3 license
    
    Resolves #15
    
    Signed-off-by: Hemant Sachdeva <hemant.evolver@gmail.com>

--- src/copyrightinserter.ts
+++ src/copyrightinserter.ts
@@ -24,6 +24,8 @@ export class CopyrightInserter {
     // 'apache' - APACHE 2.0
     // 'bsd' - BSD
     // 'mit' - MIT
+    // 'gpl3' - GPL 3.0
+    // 'agpl3' - Affero GPL 3.0
     readonly CopyrightMap = new Map([
         ['apache', (holder: string, year: string) =>
 `Copyright ${year} ${holder}
@@ -79,6 +81,22 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.`],
+
+        ['agpl3', (holder: string, year:string) =>
+`Copyright (C) ${year} ${holder}
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.`]
     ]);
 
